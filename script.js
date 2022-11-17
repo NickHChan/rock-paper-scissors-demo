8975744106cda7ca234a3461ba46bc54df33a97e
Nick <123456789+odin@users.noreply.github.com>
2022-11-16 22:10:26 -0500

    made computerpicker function more clean, added lowercase convertor in grammargethumanchoice var, added playGame function to play 1 round of rock,paper,scissors with player's promt and computer's choice

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const getComputerChoice = Math.floor(Math.random() * 3);
 const getHumanChoice = prompt('Pick Rock, Paper, or Scissors')
-const grammarGetHumanChoice = getHumanChoice.charAt(0).toUpperCase() + getHumanChoice.slice(1);
+const grammarGetHumanChoice = getHumanChoice.charAt(0).toUpperCase() + getHumanChoice.slice(1).toLowerCase();
+
 
 const computerPicker = (ranNum = getComputerChoice) => {
     let computerResult = " "
@@ -11,9 +12,31 @@ const computerPicker = (ranNum = getComputerChoice) => {
         return computerResult = "Paper"
     }
     else if(ranNum  === 2){
-        return computerResult = "Scissors"
+        return "Scissors"
     }
 }
 
-console.log(computerPicker())
-console.log(grammarGetHumanChoice)
+
+const humanChoiceChecker = (manCheck = grammarGetHumanChoice) => {
+    if(manCheck === "Rock" || manCheck === "Paper" || manCheck === "Scissors") {
+        return manCheck
+    } else {
+        return alert("Please select a valid choice, refresh page and try again.")
+    }
+}
+const humanChoice = humanChoiceChecker(grammarGetHumanChoice)
+const computerChoice = computerPicker(getComputerChoice) 
+
+const playGame = (computerChoice, humanChoice) => {
+    if (computerChoice === humanChoice) {
+        return `Tie Game, Player picked ${humanChoice}, and Computer picked ${computerChoice}`
+    } 
+    else if ( computerChoice === "Rock" && humanChoice === "Scissors" || computerChoice=== "Paper" && humanChoice === "Rock" || computerChoice === "Scissors" && humanChoice === "Paper") {
+        return `Computer WINS, ${computerChoice} beats ${humanChoice}`
+    }
+    else if (computerChoice === "Rock" && humanChoice=== "Paper" || computerChoice === "Paper" && humanChoice === "Scissors" || computerChoice === "Paper" && humanChoice === "Rock") {
+        return `Human WINS, ${humanChoice} beats ${computerChoice}`
+    }
+}
+
+console.log(playGame(computerChoice, humanChoice))
