809167e58a2d39574412a074b1e54f868c3f0380
Nick <123456789+odin@users.noreply.github.com>
2022-11-17 18:30:53 -0500

    got rid of redundant code and created play function that runs playGame function until either computer or player gets 5 wins

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-const getComputerChoice = Math.floor(Math.random() * 3);
-const getHumanChoice = prompt('Pick Rock, Paper, or Scissors')
-const grammarGetHumanChoice = getHumanChoice.charAt(0).toUpperCase() + getHumanChoice.slice(1).toLowerCase();
-
-
-const computerPicker = (ranNum = getComputerChoice) => {
-    let computerResult = " "
-    if(ranNum  === 0){
-        return computerResult = "Rock"
+const computerPicker = () => {
+    const getComputerChoice = Math.floor(Math.random() * 3);
+    if(getComputerChoice  === 0){
+        return "Rock"
     } 
-    else if(ranNum  === 1){
-        return computerResult = "Paper"
+    else if(getComputerChoice  === 1){
+        return "Paper"
     }
-    else if(ranNum  === 2){
+    else if(getComputerChoice  === 2){
         return "Scissors"
     }
 }
 
-
-const humanChoiceChecker = (manCheck = grammarGetHumanChoice) => {
-    if(manCheck === "Rock" || manCheck === "Paper" || manCheck === "Scissors") {
-        return manCheck
-    } else {
-        return alert("Please select a valid choice, refresh page and try again.")
-    }
-}
-const humanChoice = humanChoiceChecker(grammarGetHumanChoice)
-const computerChoice = computerPicker(getComputerChoice) 
-
 const playGame = (computerChoice, humanChoice) => {
+   
     if (computerChoice === humanChoice) {
         return `Tie Game, Player picked ${humanChoice}, and Computer picked ${computerChoice}`
     } 
     else if ( computerChoice === "Rock" && humanChoice === "Scissors" || computerChoice=== "Paper" && humanChoice === "Rock" || computerChoice === "Scissors" && humanChoice === "Paper") {
+        compScore += 1;
         return `Computer WINS, ${computerChoice} beats ${humanChoice}`
     }
-    else if (computerChoice === "Rock" && humanChoice=== "Paper" || computerChoice === "Paper" && humanChoice === "Scissors" || computerChoice === "Paper" && humanChoice === "Rock") {
+    else if (computerChoice === "Rock" && humanChoice=== "Paper" || computerChoice === "Paper" && humanChoice === "Scissors" || computerChoice === "Scissors" && humanChoice === "Rock") {
+        humanScore += 1;
         return `Human WINS, ${humanChoice} beats ${computerChoice}`
     }
 }
+let humanScore = 0
+let compScore = 0
 
-console.log(playGame(computerChoice, humanChoice))
+const play = () => {
+    let playerSelection = prompt(`Pick Rock, Paper, or Scissors`);
+    let playerSelectionGrammar = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let compSelection = computerPicker();
+    console.log(playGame(compSelection, playerSelectionGrammar))
+    console.log(`Your score is ${humanScore} and the computer's score is ${compScore}`)
+    if(humanScore === 5) {
+        alert(`Game Over`)
+    } else if(compScore === 5) {
+        alert(`Game Over`)
+    } else {
+        play();
+    }
+}
+play();
